013eb619d1d9be4d594273e892afd456bc523af7
Padraic Baker <pbaker@pbakerCMD6M.vmware.com>
2023-06-12 23:20:54 -0600

    test change

---
+++ WeatherForecast.cs
@@ -0,0 +1,14 @@
+namespace weather;
+
+public class WeatherForecast
+{
+    public string message = 'test';
+
+    public DateTime Date { get; set; }
+
+    public int TemperatureC { get; set; }
+
+    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+    public string? Summary { get; set; }
+}
